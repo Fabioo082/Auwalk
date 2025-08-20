a8e9856a4cee0e4841d97e3f1cd2d4634986d257
Bruno Santos <Brunomoraesleao@hotmail.com>
2025-08-20 16:14:37 -0300

    Backend adicionado em spriing boot

--- Backend/settings.gradle.kts
+++ Backend/settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "backend"
+rootProject.name = "auwalk"
 spring.datasource.url=jdbc:postgresql://localhost:5432/postgres
 spring.datasource.username=postgres
 spring.datasource.password=123
