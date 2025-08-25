864f7bfd30420b5eb313a2f132522ebc97afe484
Bruno Santos <Brunomoraesleao@hotmail.com>
2025-08-25 17:19:02 -0300

    remove all

--- composeApp/src/commonMain/kotlin/auwalk/project/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package auwalk.project
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/auwalk/project/Platform.kt
+++
@@ -1,7 +0,0 @@
-package auwalk.project
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
